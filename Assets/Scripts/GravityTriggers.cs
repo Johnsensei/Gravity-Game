5989562a0165be3265d7d5fd11698763bc180c2f
Johnsensei <johngale@Minas-Tirith.local>
2019-04-22 14:27:47 -0500

    Initial Commit
    Committing project to GitHub.

---
+++ Assets/Scripts/GravityTriggers.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityTriggers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter (Collider other){
+
+		print ("Collision detected.");
+
+		if (this.tag == "earth") {
+			Physics.gravity = new Vector3 (0, -9.81f, 0);
+		}
+
+		if (this.tag == "moon") {
+			Physics.gravity = new Vector3 (0, -1.62f, 0);
+		}
+
+		if (this.tag == "sun") {
+			Physics.gravity = new Vector3 (0, -274f, 0);
+		}
+
+		if (this.tag == "mars") {
+			Physics.gravity = new Vector3 (0, -3.711f, 0);
+		}
+
+		if (this.tag == "pluto") {
+			Physics.gravity = new Vector3 (0, -0.62f, 0);
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+	[SerializeField] GameObject earthTransporter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other){
+	
+		other.transform.position = earthTransporter.transform.position;
+		Physics.gravity = new Vector3 (0, -9.81f, 0);
+	
+	}
+}
